68cd44c1b846754bbdee1636b5ab82f698abc396
qle <dungeli3uch@gmail.com>
2025-04-11 15:13:55 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -8,8 +9,11 @@ int main(int argc, char *argv[]) {
   // Uncomment this block to pass the first stage
   printf("$ ");
 
-  // Wait for user input
+  // Wait for user input and replace the newline character with a null terminator
   char input[100];
   fgets(input, 100, stdin);
+  input[strcspn(input, "\n")] = 0;
+
+  printf("%s: command not found\n", input);
   return 0;
 }
