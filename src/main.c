6b5572402c92d85454ffc32c17867d66ac2e3c31
qle <dungeli3uch@gmail.com>
2025-04-11 16:37:17 +0900

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,33 +3,34 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+    while (1) {
+        // Flush after every printf
+        fflush(stdout);
 
-  while (1) {
-    // Flush after every printf
-    fflush(stdout);
+        // Uncomment this block to pass the first stage
+        printf("$ ");
 
-    // Uncomment this block to pass the first stage
-    printf("$ ");
+        // Wait for user input and replace the newline character with a null terminator
+        char input[100];
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = '\0';
 
-    // Wait for user input and replace the newline character with a null terminator
-    char input[100];
-    fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+		// Check for exit command
+		if (!strcmp(input, "exit 0")) {
+			return 0;
+		}
+		
+		// Extract command
+		char command[100];
+		sscanf(input, "%s", command);
 
-    // Extract first string of input
-    char command[100];
-    sscanf(input, "%s", command);
-
-    // Check for exit command
-    if (strcmp(command, "echo") == 0) {
-      printf("%s\n", input + strlen("echo") + 1);
-    } else if (strcmp(command, "exit") == 0) {
-      break; 
-    } else {
-      printf("%s: command not found\n", input);
+		// Check for built-in commands
+        if (strcmp(command, "echo") == 0) {
+            printf("%s\n", input + strlen("echo") + 1);
+        } else {
+            printf("%s: command not found\n", input);
+        }
     }
 
-  }
-
-  return 0;
+    return 0;
 }
