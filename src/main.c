6856f2d981bd05ea5ad9ea33d11303d1e6353a24
qle <dungeli3uch@gmail.com>
2025-04-11 17:17:30 +0900

    Implemented type command to check if a command is a builtin command

--- src/main.c
+++ src/main.c
@@ -10,26 +10,42 @@ int main(int argc, char *argv[]) {
         // Uncomment this block to pass the first stage
         printf("$ ");
 
-        // Wait for user input and replace the newline character with a null terminator
+        // Wait for user input and replace the newline with null terminator
         char input[100];
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
 
 		// Check for exit command
-		if (!strcmp(input, "exit 0")) {
+		if (!strcmp(input, "exit") | !strcmp(input, "exit 0")) {
 			return 0;
 		}
 		
-		// Extract command
+		// List of builtin commands
+		char builtins[][16] = {"exit", "exit 0", "echo", "type"};
+		// Extract command from input
 		char command[100];
 		sscanf(input, "%s", command);
 
-		// Check for built-in commands
+		// Execute the command
         if (strcmp(command, "echo") == 0) {
-            printf("%s\n", input + strlen("echo") + 1);
-        } else {
-            printf("%s: command not found\n", input);
-        }
+			printf("%s\n", input + strlen("echo") + 1);
+
+        } else if (strcmp(command, "type") == 0) {
+			// Check if the command is a builtin			
+			char *command_type = input + strlen("type") + 1;
+			int builtin_arr_size = sizeof(builtins) / sizeof(builtins[0]);			
+			for (int i = 0; i < builtin_arr_size; i++) {
+				if (strcmp(command_type, builtins[i]) == 0) {
+					printf("%s is a shell builtin\n", builtins[i]);
+					break;
+				} else if (i == builtin_arr_size - 1) {
+					printf("%s: not found\n", command_type);
+				}
+			}
+
+		} else {
+			printf("%s: command not found\n", input);
+		}
     }
 
     return 0;
